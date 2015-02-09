2b4f04c64f9be723e7e72d3d9b8999714badd6b1
Ben <tuck0046@algonquinlive.com>
2015-02-08 23:37:00 -0500

    Scale Randomizer

---
+++ MapMapTrunk/Assets/IconAnimator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IconAnimator : MonoBehaviour 
+{
+	public float speed = 0.1f;
+	public float MaxSize = 0.0f;
+	public float MinSize = 0.0f;
+	public float x,y,z;
+	public float maxScale = 0.5f;
+	public float minScale = 2.0f;
+	public float shrinkSpeed = 1.0f;    
+	
+	private float targetScale;
+	private Vector3 v3Scale;
+	// Use this for initialization
+	void Start () 
+	{
+		v3Scale = transform.localScale; 
+		GeneratedPosition();
+		transform.localScale.Equals(Random.Range(MinSize,MaxSize));
+	}
+	Vector3 GeneratedPosition()
+	{
+
+		x = Random.Range(MinSize,MaxSize);
+		y = Random.Range(MinSize,MaxSize);
+		z = Random.Range(MinSize,MaxSize);
+		return new Vector3(x,y,z);
+	}
+
+	void Shrink()
+	{
+		targetScale = minScale;
+		v3Scale = new Vector3(targetScale, targetScale, targetScale);
+		if(v3Scale.x == minScale)
+		{
+			Grow();
+		}
+	}
+
+	void Grow()
+	{
+		targetScale = maxScale;
+		v3Scale = new Vector3(targetScale, targetScale, targetScale);
+		if(v3Scale.x == maxScale)
+		{
+			Shrink();
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		Shrink();
+		transform.localScale = Vector3.Lerp(transform.localScale, v3Scale, Time.deltaTime*shrinkSpeed);
+	
+	}
+}
---
+++ MapMapTrunk/Assets/RandomMaterial.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var material1 : Material; var material2 : Material;
+
+function OnChangeMaterial() {    // toggle between the two materials   
+ if( renderer.material == material1 )      
+ renderer.material = material2;   else      renderer.material = material1;
+
+}
+
+function Start () {
+	OnChangeMaterial();
+}
+
+function Update () {
+
+}
