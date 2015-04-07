bed6d0722c489b9f83f9767d5e79b29336ef45fe
Ben <tuck0046@algonquinlive.com>
2015-04-07 16:23:24 -0400

    Updated to Unity 5

--- MapMapTrunk/Assets/Scripts/RandomMaterial.js
+++ MapMapTrunk/Assets/Scripts/RandomMaterial.js
@@ -3,8 +3,8 @@
 var material1 : Material; var material2 : Material;
 
 function OnChangeMaterial() {    // toggle between the two materials   
- if( renderer.material == material1 )      
- renderer.material = material2;   else      renderer.material = material1;
+ if( GetComponent.<Renderer>().material == material1 )      
+ GetComponent.<Renderer>().material = material2;   else      GetComponent.<Renderer>().material = material1;
 
 }
 
